5c26a4e45a87fe2fc2c474ad3477e1d5a5a1867b
Damion Vega <damionvega333@gmail.com>
2021-02-17 10:34:09 -0800

    Add @types/jest & babel-plugin-rewire to devDeps

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,5 @@
 module.exports = {
   presets: [["@babel/preset-env", { targets: { node: "current" } }]],
+  // Allow non-exported functions from a module to be testable https://github.com/jhnns/rewire
+  plugins: ["babel-plugin-rewire"],
 };

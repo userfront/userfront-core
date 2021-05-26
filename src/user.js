66f20f0763eb42163a623e977e260cfada10051e
Damion Vega <damionvega333@gmail.com>
2021-05-25 17:11:22 -0700

    Refactor apiResource.update to user

--- src/user.js
+++ src/user.js
@@ -1,20 +1,28 @@
+import axios from "axios";
+import jwt from "jsonwebtoken";
+import { apiUrl } from "./constants.js";
+
 /**
- * Create a user object based on tokens
- * @param {Object} tokens
- * @property {Object} idToken Decoded ID token
- * @property {Object} accessToken Decoded access token
+ * Create a user object based on ID token
+ * @param {Object} options
+ * @property {Object} options.store Userfront store
+ * @property {Function} options.afterUpdate Function to call after update is finished
  * @returns {Object}
  */
-export default (tokens = {}) => {
-  const { idToken, accessToken } = tokens;
-  if (!idToken || !accessToken) {
-    throw new Error("User error: Missing token information");
+export default ({ store, afterUpdate }) => {
+  if (!store.idToken) {
+    throw new Error("User: Missing ID token");
+  }
+  if (!store.accessToken) {
+    throw new Error("User: Missing ID token");
   }
 
   const user = {};
+  const decodedIdToken = jwt.decode(store.idToken);
+  const decodedAccessToken = jwt.decode(store.accessToken);
 
   // Set basic user information properties from ID token
-  for (const prop of [
+  const idTokenProps = [
     "email",
     "username",
     "name",
@@ -22,19 +30,74 @@ export default (tokens = {}) => {
     "confirmedAt",
     "createdAt",
     "updatedAt",
-  ]) {
-    user[prop] = idToken[prop];
+    "mode",
+    "userId",
+    "userUuid",
+    "tenantId",
+    "isConfirmed",
+  ];
+  for (const prop of idTokenProps) {
+    user[prop] = decodedIdToken[prop];
   }
 
   // Unnest user data properties if present
-  for (const prop of Object.keys(idToken.data)) {
-    user[prop] = idToken.data[prop];
+  for (const prop of Object.keys(decodedIdToken.data)) {
+    user[prop] = decodedIdToken.data[prop];
   }
 
-  // Set user authorization properties from access token
-  for (const prop of ["userId", "authorization"]) {
-    user[prop] = accessToken[prop];
-  }
+  return {
+    ...user,
+    /**
+     * Update user information via Userfront API
+     * @param {Object} updates User properties to update e.g. { name: 'John Doe' }
+     * @returns {Promise<void>}
+     */
+    async update(updates) {
+      if (!decodedAccessToken.userId) {
+        throw new Error("API resource update error: Missing ID");
+      }
 
-  return user;
+      const payload = {
+        ...updates,
+      };
+
+      // Restructure data updates
+      if (updates.hasOwnProperty("data")) {
+        delete payload.data;
+        payload.data = {
+          data: updates.data,
+        };
+      }
+
+      // Restructure any properties outside of default ID token properties into data object
+      for (const prop in updates) {
+        if (!idTokenProps.includes(prop) && prop !== "data") {
+          if (!updates.hasOwnProperty("data")) {
+            payload.data = {};
+          }
+
+          payload.data[prop] = updates[prop];
+          delete payload[prop];
+        }
+      }
+
+      try {
+        await axios.put({
+          url: `${apiUrl}tenants/${store.tenantId}/users/${decodedAccessToken.userId}`,
+          headers: {
+            authorization: `Bearer ${store.accessToken}`,
+          },
+          payload,
+        });
+      } catch (error) {
+        throw new Error(error.message);
+      }
+
+      if (afterUpdate && typeof afterUpdate === "function") {
+        afterUpdate();
+      }
+
+      return Promise.resolve();
+    },
+  };
 };
--- test/user.spec.js
+++ test/user.spec.js
@@ -1,64 +1,25 @@
 import jwt from "jsonwebtoken";
 import axios from "axios";
 
+import utils from "./utils";
 import { apiUrl } from "../src/constants.js";
 import Userfront from "../src/index.js";
 import user from "../src/user.js";
 
 jest.mock("axios");
-
 const tenantId = "abcdefgh";
 
 describe("User", () => {
-  // Make available for testing
+  // Make non-exported functions available for testing
   Userfront.setCookiesAndTokens = Userfront.__get__("setCookiesAndTokens");
-
-  const accessTokenUserDefaults = {
-    userId: 3,
-    userUuid: "aaaa-bbbb-cccc-dddd",
-    isConfirmed: true,
-    authorization: {
-      [tenantId]: {
-        roles: ["member"],
-      },
-    },
-    sessionId: "bbbb-cccc-dddd-eeee",
-  };
-
-  const idTokenUserDefaults = {
-    name: "John Doe",
-    email: "johndoe@example.com",
-    username: "johndoe",
-    image: "https://example.com/profile-image.png",
-    data: {
-      age: 22,
-      color: "blue",
-      birthdate: new Date(),
-      data: {
-        country: "Monoco",
-      },
-      metadata: {
-        lastLogin: new Date(),
-      },
-    },
-    confirmedAt: new Date(),
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  };
+  Userfront.setUser = Userfront.__get__("setUser");
 
   beforeAll(() => {
     Userfront.init(tenantId);
 
     // Create and set access & ID tokens
-    const accessToken = jwt.sign(
-      {
-        mode: "test",
-        tenantId,
-        ...accessTokenUserDefaults,
-      },
-      "accessTokenSecret"
-    );
-    const idToken = jwt.sign(idTokenUserDefaults, "idTokenSecret");
+    const accessToken = utils.createAccessToken();
+    const idToken = utils.createIdToken();
 
     Userfront.setCookiesAndTokens({
       access: {
@@ -73,102 +34,144 @@ describe("User", () => {
     });
   });
 
-  describe("constructor", () => {
-    it("should set user's information", () => {
-      const ufUser = user({
-        idToken: jwt.decode(Userfront.store.idToken),
-        accessToken: jwt.decode(Userfront.store.accessToken),
-      });
-
+  describe("user constructor", () => {
+    it("should set user's information based on ID token and return user object", () => {
       const parsedUser = {
-        ...JSON.parse(JSON.stringify(idTokenUserDefaults)),
-        ...JSON.parse(JSON.stringify(idTokenUserDefaults.data)),
+        ...JSON.parse(JSON.stringify(utils.idTokenUserDefaults)),
+        ...JSON.parse(JSON.stringify(utils.idTokenUserDefaults.data)),
       };
 
-      for (const prop in idTokenUserDefaults) {
+      const ufUser = user({ store: Userfront.store });
+
+      // Assert ufUser values were set correctly in user() constructor
+      for (const prop in utils.defaultIdTokenProperties) {
         expect(parsedUser[prop]).toEqual(ufUser[prop]);
       }
     });
   });
 
-  it("should get user's information", () => {
-    const parsedUser = {
-      ...JSON.parse(JSON.stringify(idTokenUserDefaults)),
-      ...JSON.parse(JSON.stringify(idTokenUserDefaults.data)),
-    };
+  describe("user object", () => {
+    it("should get user's information", () => {
+      const parsedUser = {
+        ...JSON.parse(JSON.stringify(utils.idTokenUserDefaults)),
+        ...JSON.parse(JSON.stringify(utils.idTokenUserDefaults.data)),
+      };
 
-    for (const prop in idTokenUserDefaults) {
-      expect(parsedUser[prop]).toEqual(Userfront.store.user[prop]);
-    }
+      // ufUser has been updated via ID token in beforeAll
+      const ufUser = Userfront.user;
+
+      // ufUser values from ID token should match the defaults given
+      for (const prop in utils.defaultIdTokenProperties) {
+        expect(parsedUser[prop]).toEqual(ufUser[prop]);
+      }
+    });
   });
 
-  it("should update user's information via API and call refresh()", async () => {
-    const updates = {
-      username: "john-doe-updated",
-      data: {
-        country: "Spain",
-      },
-    };
+  describe("user.update()", () => {
+    it("should call afterUpdate hook after updating", async () => {
+      const updates = {
+        name: "Jane Doe",
+      };
 
-    // Create mock tokens & refresh response
-    const accessToken = jwt.sign(
-      {
-        mode: "test",
-        tenantId,
-        ...accessTokenUserDefaults,
-      },
-      "updatedAccessTokenSecret"
-    );
-    const idToken = jwt.sign(
-      {
-        ...idTokenUserDefaults,
-        ...updates,
-      },
-      "updatedIdTokenSecret"
-    );
-    axios.get.mockImplementationOnce(() =>
-      Promise.resolve({
-        data: {
-          tokens: {
-            access: {
-              value: accessToken,
-            },
-            id: {
-              value: idToken,
-            },
-            refresh: {
-              value: "",
-            },
+      // Manually assign user object and pass in spy in place of `Userfront.refresh()`
+      const refresh = jest.fn();
+      Userfront.store.user = user({
+        store: Userfront.store,
+        afterUpdate: refresh,
+      });
+
+      // Update user via Userfront API
+      await Userfront.user.update(updates);
+
+      // Should have made "update user" API request
+      const { userId } = jwt.decode(Userfront.store.accessToken);
+      expect(axios.put).toBeCalledWith({
+        url: `${apiUrl}tenants/${tenantId}/users/${userId}`,
+        headers: {
+          authorization: `Bearer ${Userfront.store.accessToken}`,
+        },
+        payload: updates,
+      });
+
+      // Should have called `afterUpdate` function
+      expect(refresh).toHaveBeenCalledTimes(1);
+
+      // Revert Userfront.user without refresh spy
+      Userfront.setUser();
+    });
+
+    it("should update user's information via API", async () => {
+      const updates = {
+        username: "john-doe-updated",
+        country: "Spain",
+      };
+
+      // Create mock tokens & refresh response
+      const accessToken = utils.createAccessToken();
+      const idToken = jwt.sign(
+        {
+          ...utils.idTokenUserDefaults,
+          ...utils.sharedTokenProperties.token,
+          username: updates.username,
+          data: {
+            country: updates.country,
           },
         },
-      })
-    );
+        utils.randomString()
+      );
+      axios.post.mockImplementationOnce(() =>
+        Promise.resolve({
+          data: {
+            tokens: {
+              access: {
+                value: accessToken,
+              },
+              id: {
+                value: idToken,
+              },
+              refresh: {
+                value: "",
+              },
+            },
+          },
+        })
+      );
+      const originalTokens = {
+        idToken: Userfront.store.idToken,
+        accessToken: Userfront.store.accessToken,
+      };
 
-    const originalTokens = {
-      idToken: Userfront.store.idToken,
-      accessToken: Userfront.store.accessToken,
-    };
-    await Userfront.store.user.update(updates);
+      // Update user via Userfront API
+      await Userfront.user.update(updates);
 
-    const { userId } = Userfront.store.user;
-    expect(axios.put).toBeCalledWith({
-      url: `${apiUrl}tenants/${tenantId}/users/${userId}`,
-      headers: {
-        authorization: `Bearer ${originalTokens.accessToken}`,
-      },
-      payload: updates,
+      // Should have made "update user" API request
+      const { userId } = jwt.decode(Userfront.store.accessToken);
+      expect(axios.put).toBeCalledWith({
+        url: `${apiUrl}tenants/${tenantId}/users/${userId}`,
+        headers: {
+          authorization: `Bearer ${originalTokens.accessToken}`,
+        },
+        payload: {
+          username: updates.username,
+          data: {
+            country: updates.country,
+          },
+        },
+      });
+
+      // Assert user was updated
+      expect(Userfront.user.username).toEqual(updates.username);
+      expect(Userfront.user.country).toEqual(updates.country);
+      expect(Userfront.user.age).toBeUndefined;
+      expect(Userfront.user.color).toBeUndefined;
+      expect(Userfront.user.birthdate).toBeUndefined;
+      expect(Userfront.user.metadata).toBeUndefined;
+
+      // Assert tokens were updated
+      expect(originalTokens.idToken).not.toEqual(Userfront.store.idToken);
+      expect(originalTokens.accessToken).not.toEqual(
+        Userfront.store.accessToken
+      );
     });
-    expect(axios.get).toBeCalledWith({
-      url: `${apiUrl}tenants/${tenantId}/refresh`,
-      headers: {
-        authorization: `Bearer ${originalTokens.accessToken}`,
-      },
-    });
-
-    expect(Userfront.store.user.username).toEqual(updates.username);
-    expect(Userfront.store.user.country).toEqual(updates.data.country);
-
-    expect(originalTokens.idToken).not.toEqual(Userfront.store.idToken);
-    expect(originalTokens.accessToken).not.toEqual(Userfront.store.accessToken);
   });
 });

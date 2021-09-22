dec2b0887fb997a37639e1474c7b7f17bb5592fb
Tyler <tyler@anymod.com>
2021-09-21 17:17:09 -0700

    Add d.ts file

--- ts/index.d.ts
+++ ts/index.d.ts
@@ -1,42 +1,75 @@
-declare function P(): Promise<any>;
-declare namespace a {
-    export namespace user {
-        export function update(e: object): Promise<void> | Promise<{}>;
-    }
-}
-export declare function addInitCallback(e: any): void;
-export declare function init(e: any, r?: {}): void;
-export declare function registerUrlChangedEventListener(): void;
+// init
+export declare function init(tenantId: string, options?: object): Promise<void>;
+
+// addInitCallback()
+export declare function addInitCallback(callback: Function): void;
+
+// signup()
+export declare function signup({ method, email, username, name, password, redirect }: {
+    method: string;
+    email?: string;
+    username?: string;
+    name?: string;
+    password?: string;
+    redirect?: string | boolean;
+}): Promise<void>;
+
+// login()
+export declare function login({ method, email, username, emailOrUsername, password, token, uuid, redirect }: {
+    method: string;
+    email?: string;
+    username?: string;
+    emailOrUsername?: string;
+    password?: string;
+    token?: string;
+    uuid?: string;
+    redirect?: string | boolean;
+}): Promise<void>;
+
+// logout()
 export declare function logout(): Promise<void>;
-export declare function setMode(): Promise<void>;
-export declare function login({ method: e, email: r, username: o, emailOrUsername: i, password: c, token: u, uuid: d, redirect: h }?: {
-    method: string;
-    email: string;
-    username: string;
-    emailOrUsername: string;
-    password: string;
-    token: string;
-    uuid: string;
-    redirect: boolean;
-}): Promise<any>;
-export declare function resetPassword({ uuid: e, token: r, password: o, redirect: i }: {
-    uuid: string;
-    token: string;
-    password: string;
-    redirect: boolean;
-}): Promise<any>;
-export declare function sendLoginLink(e: any): Promise<any>;
-export declare function sendResetLink(e: any): Promise<any>;
-export declare function signup({ method: e, username: r, name: o, email: i, password: s, data: c, redirect: u }?: {
-    method: string;
-    username: string;
-    name: string;
-    email: string;
-    password: string;
-    data: any;
-    redirect: boolean;
-}): Promise<any>;
-export declare function accessToken(): any;
-export declare function idToken(): any;
+
+// redirectIfLoggedIn()
 export declare function redirectIfLoggedIn(): Promise<void>;
-export { P as refresh, a as store };
+
+// resetPassword()
+export declare function resetPassword({ password, token, uuid, redirect }: {
+    password: string;
+    token?: string;
+    uuid?: string;
+    redirect?: string | boolean;
+}): Promise<void>;
+
+// sendLoginLink()
+export declare function sendLoginLink(email: string): Promise<void>;
+
+// sendResetLink()
+export declare function sendResetLink(email: string): Promise<void>;
+
+// user
+interface User {
+    email?: string;
+    name?: string;
+    image?: string;
+    data?: object;
+    username?: string;
+    confirmedAt?: string;
+    isConfirmed?: boolean;
+    createdAt?: string;
+    updatedAt?: string;
+    mode?: 'live' | 'test';
+    tenantId?: string;
+    userId?: number;
+    userUuid?: string;
+    update?: Function;
+}
+export declare const user: User;
+
+// tokens
+interface Tokens {
+    accessToken: string;
+    accessTokenName: string;
+    idToken: string;
+    idTokenName: string;
+}
+export declare const tokens: Tokens

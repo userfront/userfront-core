a4a5660b227293ca4295328e8f9a3d8fdcacb23a
Ryan Felix <ryan@userfront.com>
2023-01-27 15:33:58 -0500

    Add jsdocs

--- src/password.js
+++ src/password.js
@@ -63,7 +63,18 @@ export async function signupWithPassword({
 /**
  * Log a user in with email/username and password.
  * Redirect the browser after successful login based on the redirectTo value returned.
- * @param {Object} options
+ * @param {Object} params
+ * @param {string} params.email The user's email. One of email/username/emailOrUsername should be present.
+ * @param {string} params.username The user's username. One of email/username/emailOrUsername should be present.
+ * @param {string} params.emailOrUsername Either the user's email or username. One of email/username/emailOrUsername should be present.
+ * @param {string} params.password
+ * @param {string | boolean} params.redirect 
+ *  URL to redirect to after login, or false to suppress redirect. Otherwise, redirects to the after-login path set on the server.
+ * @param {object} params.options
+ * @param {boolean} params.options.noResetEmail
+ *  By default, Userfront sends a password reset email if a user without a password tries to log in with a password.
+ *  Set options.noResetEmail = true to override this behavior and return an error instead.
+ * 
  */
 export async function loginWithPassword({
   email,
